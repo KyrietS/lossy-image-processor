d8c833bd84bdd61633f95814801ddf5712d2f9ba
Kyriet <kyriet.sebastian@gmail.com>
2020-05-27 21:50:33 +0200

    Added merging to band splitter. Fixed splitting.

--- LIP_Core/src/BandSplitter.cpp
+++ LIP_Core/src/BandSplitter.cpp
@@ -1,5 +1,7 @@
 #include "BandSplitter.hpp"
 
+#include <cassert>
+
 using byte_t = uint8_t;
 
 BandSplitter::BandSplitter(const std::vector<byte_t>& data)
@@ -9,18 +11,70 @@ BandSplitter::BandSplitter(const std::vector<byte_t>& data)
 
 void BandSplitter::split(const std::vector<byte_t>& data)
 {
-    if (data.size() == 0)  
+    if (data.size() == 0)
         return;
 
-    upper_band.resize(data.size());
-    lower_band.resize(data.size());
+    lower_band.clear();
+    upper_band.clear();
 
-    upper_band[0] = (float)data[0] / 2;
-    lower_band[0] = (float)data[0] / 2;
-
-    for (size_t i = 1; i < data.size(); i++)
+    for (size_t i = 1; i < data.size(); i += 2)
     {
-        lower_band[i] = ((float)data[i] + (float)data[i - 1]) / 2.0f;
-        upper_band[i] = ((float)data[i] - (float)data[i - 1]) / 2.0f;
+        float lower = ((float)data[i] + (float)data[i - 1]) / 2.0f;
+        float upper = ((float)data[i] - (float)data[i - 1]) / 2.0f;
+        lower_band.push_back(lower);
+        upper_band.push_back(upper);
     }
+
+    // If data is not even sized
+    if (data.size() % 2 != 0)
+    {
+        float lower = (float)data[data.size() - 1] / 2.0f;
+        float upper = -1 * (float)data[data.size() - 1] / 2.0f;
+        lower_band.push_back(lower);
+        upper_band.push_back(upper);
+    }
+}
+
+std::vector<byte_t> BandSplitter::mergeEven()
+{
+    return merge(true);
+}
+
+std::vector<byte_t> BandSplitter::mergeUneven()
+{
+    return merge(false);
+}
+
+std::vector<byte_t> BandSplitter::merge(bool even_result)
+{
+    assert(lower_band.size() == upper_band.size());
+    const size_t BAND_SIZE = lower_band.size();
+    std::vector<byte_t> result;
+
+    if (even_result)
+    {
+        for (int i = 0; i < BAND_SIZE; i++)
+        {
+            float even = lower_band[i] - upper_band[i];
+            float uneven = lower_band[i] + upper_band[i];
+
+            result.push_back((byte_t)even);
+            result.push_back((byte_t)uneven);
+        }
+    }
+    else if (BAND_SIZE > 0)
+    {
+        for (int i = 0; i < BAND_SIZE - 1; i++)
+        {
+            float even = lower_band[i] - upper_band[i];
+            float uneven = lower_band[i] + upper_band[i];
+
+            result.push_back((byte_t)even);
+            result.push_back((byte_t)uneven);
+        }
+        float last_element = lower_band[BAND_SIZE - 1] - upper_band[BAND_SIZE - 1];
+        result.push_back((byte_t)last_element);
+    }
+
+    return result;
 }
